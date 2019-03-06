6ce73a8765f62314f74682db83c9e858dcb3924d
Cait Sidener <cait.sidener@gmail.com>
2019-03-05 18:26:29 -0600

    setting up files

---
+++ db/seeds.sql
@@ -0,0 +1,6 @@
+INSERT INTO burgers(burger_name, devoured) VALUES ('Classic', true);
+INSERT INTO burgers(burger_name, devoured) VALUES ('Veggie', true);
+INSERT INTO burgers(burger_name, devoured) VALUES ('Fried Frog', false);
+INSERT INTO burgers(burger_name, devoured) VALUES ('Cronut', false);
+INSERT INTO burgers(burger_name, devoured) VALUES ('Mac and Cheese', false);
+INSERT INTO burgers(burger_name, devoured) VALUES ('Hot Fudge Sundae', false);
