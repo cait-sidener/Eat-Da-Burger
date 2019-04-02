a5572e7f0563d9a264ec9a8952871fee67599221
Cait Sidener <cait.sidener@gmail.com>
2019-04-02 18:31:19 -0500

    fixes

--- server.js
+++ server.js
@@ -1,6 +1,6 @@
 var express = require("express");
 
-var PORT = process.env.PORT || 8080;
+var PORT = process.env.PORT || 3000;
 
 var app = express();
 
@@ -22,8 +22,14 @@ var routes = require("./controllers/burgersController.js");
 
 app.use(routes);
 
-// Start our server so that it can begin listening to client requests.
-app.listen(PORT, function() {
-  // Log (server-side) when our server has started
-  console.log("Server listening on: http://localhost:" + PORT);
+process.on('uncaughtException', function (err) {
+  console.log(err);
 });
+
+// // Start our server so that it can begin listening to client requests.
+// app.listen(PORT, function() {
+//   // Log (server-side) when our server has started
+//   console.log("Server listening on: http://localhost:" + PORT);
+// });
+
+app.listen(port);
