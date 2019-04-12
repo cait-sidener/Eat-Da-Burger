9a6ca033a33f28972e3ef55c2d3214b27e65781c
Cait Sidener <cait.sidener@gmail.com>
2019-04-11 19:58:42 -0500

    fix error

--- server.js
+++ server.js
@@ -26,10 +26,8 @@ process.on('uncaughtException', function (err) {
   console.log(err);
 });
 
-// // Start our server so that it can begin listening to client requests.
-// app.listen(PORT, function() {
-//   // Log (server-side) when our server has started
-//   console.log("Server listening on: http://localhost:" + PORT);
-// });
-
-app.listen(port);
+// Start our server so that it can begin listening to client requests.
+app.listen(PORT, function() {
+  // Log (server-side) when our server has started
+  console.log("Server listening on: http://localhost:" + PORT);
+});
